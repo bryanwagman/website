036fe31251aa0df3a5a4107edd127e6e9a248964
Bryan Wagman <bryanwagman@bryanwagman.com>
2024-02-21 17:19:30 -0500

    feat: add theme detection to badge image

--- app/_components/Availability/index.tsx
+++ app/_components/Availability/index.tsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from 'react'
 import CalendlyPopupText from '@/components/CalendlyPopupText'
 import Image from 'next/image'
 import PipedriveWebForm from '@/components/PipedriveWebForm'
-import memberBadge from './fu-members-badge.png'
+import memberBadgeDark from './fu-members-badge-dark.png'
+import memberBadgeLight from './fu-members-badge-light.png'
 import styles from './Availability.module.scss'
 
 const THRESHOLD: number = 30 * 24 * 60 * 60 * 1000 // thirty days
@@ -42,9 +43,15 @@ export default function Availability({
           <a href="https://www.freelancersunion.org?utm_source=badge&utm_campaign=member&utm_content=member-stamp-200">
             <Image
               alt="Proud member of Freelancers Union"
-              className={styles['Availability-badgeImage']}
+              className={`${styles['Availability-badgeImage']} ${styles['Availability-badgeImage--light']}`}
               priority={true}
-              src={memberBadge}
+              src={memberBadgeLight}
+            />
+            <Image
+              alt="Proud member of Freelancers Union"
+              className={`${styles['Availability-badgeImage']} ${styles['Availability-badgeImage--dark']}`}
+              priority={true}
+              src={memberBadgeDark}
             />
           </a>
         </div>
