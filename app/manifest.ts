7761bdd62b841deef54d97f6030055e57577a835
Bryan Wagman <bryanwagman@bryanwagman.com>
2024-02-21 15:50:50 -0500

    feat: add maskable icons at the minimum size to meet installability requirements

--- app/manifest.ts
+++ app/manifest.ts
@@ -4,15 +4,24 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     background_color: '#f5f5f5',
     description: 'Software developer based in New York City',
+    display: 'minimal-ui',
     icons: [
       {
+        purpose: 'maskable',
         sizes: 'any',
         src: '/favicon.ico',
         type: 'image/x-icon',
       },
       {
+        purpose: 'maskable',
         sizes: '192x192',
         src: '/apple-icon.png',
+        type: 'image/png',
+      },
+      {
+        purpose: 'maskable',
+        sizes: '512x512',
+        src: '/icon.png',
         type: 'image/png',
       },
     ],
